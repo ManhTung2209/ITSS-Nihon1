97530df72ab9c9313025073320f3d0a52c4aeeec
Dien <diennd2232004@gmail.com>
2025-11-28 08:53:01 +0700

    Update CafeDTO

---
+++ Back-end/cafe-finder-backend/src/main/java/dto/CafeDTO.java
@@ -0,0 +1,44 @@
+package dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+public class CafeDTO {
+    private Integer cafeID;
+    private String name;
+    private String address;
+    private BigDecimal rating;
+
+    public Integer getCafeID() {
+        return cafeID;
+    }
+
+    public void setCafeID(Integer cafeID) {
+        this.cafeID = cafeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public BigDecimal getRating() {
+        return rating;
+    }
+
+    public void setRating(BigDecimal rating) {
+        this.rating = rating;
+    }
+}
